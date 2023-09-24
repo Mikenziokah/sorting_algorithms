7e612f7cef6d6bd63b6416706a381c0bffc82fbc
koome <koomemc@gmail.com>
2023-09-24 16:04:13 -0700

    quick sort

--- sort.h
+++ sort.h
@@ -18,11 +18,14 @@ typedef struct listint_s
 	struct listint_s *next;
 
 } listint_t;
-
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void quick_recursive(int *array, size_t start, size_t pivot, size_t size);
+size_t partition(int *array, size_t compare, size_t pivot, size_t size);
 #endif
